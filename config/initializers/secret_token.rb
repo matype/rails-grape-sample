2f00b426bb27061125340187f869bd20c1977760
Masaaki Morishita <ikasama3.0@gmail.com>
2014-03-12 00:06:56 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsGrapeSample::Application.config.secret_key_base = 'ff7bcf7db68da0b1fdc90fd185a1fe530aec4a0317c0d2f2bb11b640a598c95dfd1ce6b838a70e29f78f7ebfde1a64eff7954d9a50cfb1d8bb32d50071fa95e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsGrapeSample::Application.config.session_store :cookie_store, key: '_rails_grape_sample_session'
